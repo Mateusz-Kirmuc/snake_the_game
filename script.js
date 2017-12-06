711dd746f54e44eac82e0f5a8c1171d927850bb1
mateusz <mateusz@kirmuc.pl>
2017-12-06 09:20:08 +0100

    Implement snake display using observable array

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ var AppViewModel = function() {
   self.boardSizes = ["small", "medium", "large"];
   self.selectedBoardSize = ko.observable();
   self.screenWidth = ko.observable($(window).width());
+  self.snakeBody = ko.observableArray([
+    {row: 0, cell: 0},
+    {row: 0, cell: 1},
+    {row: 0, cell: 2},
+    {row: 0, cell: 3},
+    {row: 0, cell: 4},
+    {row: 0, cell: 5}
+  ])
+
   self.changeBoardSize = function() {
     if (self.selectedBoardSize() == "small") {
       self.numberOfCellInRow(30);
@@ -16,6 +25,14 @@ var AppViewModel = function() {
     if (self.selectedBoardSize() == "large") {
       self.numberOfCellInRow(90);
     }
+  }
+  self.isInSnakeBody = function(row, cell) {
+    for (var snakeCell of this.snakeBody()) {
+      if (row == snakeCell.row && cell == snakeCell.cell){
+        return true;
+      }
+    }
+    return false;
   }
 };
 var appViewModel = new AppViewModel();
